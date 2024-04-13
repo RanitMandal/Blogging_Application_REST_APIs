f0caa15af1019f53fe7731834e233847978ec6dd
Ranit Mandal <ranitmandal17@gmail.com>
2024-04-13 15:23:42 +0530

    update security

--- src/main/java/com/blog/api/config/SecurityConfig.java
+++ src/main/java/com/blog/api/config/SecurityConfig.java
@@ -6,32 +6,37 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
-	protected void confgure(HttpSecurity http) throws Exception {
-		
-		
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 		.csrf()
 		.disable()
-		.authorizeRequests()
+		.authorizeHttpRequests()
 		.anyRequest()
 		.authenticated()
 		.and()
 		.httpBasic();
 		
-	
+		http.addFilterBefore(null, null);
 		
+		DefaultSecurityFilterChain defaultSecurityFilterChain = http.build();
+		
+		
+		
+		return defaultSecurityFilterChain;
 	}
 
 }
